dcd8e2ea22518cbb8ea135379ff447a71cbd7e47
github-action-benchmark <github@users.noreply.github.com>
2026-07-24 19:11:05 +0000

    add BenchmarkDotNet (benchmarkdotnet) benchmark result for d51d534e632037992f32e2cc855301241c484bd7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781975270309,
+  "lastUpdate": 1784920265626,
   "repoUrl": "https://github.com/Chris-Wolfgang/String-Extensions",
   "entries": {
     "BenchmarkDotNet": [
@@ -96,6 +96,102 @@ window.BENCHMARK_DATA = {
             "value": 102.61174432436626,
             "unit": "ns",
             "range": "± 0.49151799683668546"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "210299580+Chris-Wolfgang@users.noreply.github.com",
+            "name": "Chris Wolfgang",
+            "username": "Chris-Wolfgang"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d51d534e632037992f32e2cc855301241c484bd7",
+          "message": "Merge pull request #162 from Chris-Wolfgang/dependabot/github_actions/github-actions-f580fb19b9\n\nBump the github-actions group across 1 directory with 5 updates",
+          "timestamp": "2026-07-24T15:08:07-04:00",
+          "tree_id": "43a08560cbc071b1455d71c304861f2fb469343a",
+          "url": "https://github.com/Chris-Wolfgang/String-Extensions/commit/d51d534e632037992f32e2cc855301241c484bd7"
+        },
+        "date": 1784920264256,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.CasingBenchmarks.ToTitleCase",
+            "value": 289.10956716537476,
+            "unit": "ns",
+            "range": "± 2.6905660230587305"
+          },
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.CasingBenchmarks.ToCamelCase",
+            "value": 94.82506692409515,
+            "unit": "ns",
+            "range": "± 2.226404210066388"
+          },
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.CasingBenchmarks.ToPascalCase",
+            "value": 101.95662248134613,
+            "unit": "ns",
+            "range": "± 0.36032896100867123"
+          },
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.CasingBenchmarks.ToKebabCase",
+            "value": 90.92079134782155,
+            "unit": "ns",
+            "range": "± 0.6526000219375238"
+          },
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.CasingBenchmarks.ToSnakeCase",
+            "value": 91.8283342520396,
+            "unit": "ns",
+            "range": "± 0.7591302616944898"
+          },
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.LeftRightBenchmarks.Substring_Left",
+            "value": 8.872750729322433,
+            "unit": "ns",
+            "range": "± 0.14478218191815553"
+          },
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.LeftRightBenchmarks.Left",
+            "value": 8.998756711681684,
+            "unit": "ns",
+            "range": "± 0.10303309442385429"
+          },
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.LeftRightBenchmarks.Substring_Right",
+            "value": 8.580599760015806,
+            "unit": "ns",
+            "range": "± 0.13979921295608924"
+          },
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.LeftRightBenchmarks.Right",
+            "value": 11.221238364775976,
+            "unit": "ns",
+            "range": "± 0.05447289307493849"
+          },
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.PadCenterBenchmarks.Manual_PadLeftPadRight",
+            "value": 43.28969379266103,
+            "unit": "ns",
+            "range": "± 1.5252067676129022"
+          },
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.PadCenterBenchmarks.PadCenter",
+            "value": 96.78382543722789,
+            "unit": "ns",
+            "range": "± 3.175626691039455"
+          },
+          {
+            "name": "Wolfgang.Extensions.String.Benchmarks.PadCenterBenchmarks.PadCenter_WithChar",
+            "value": 97.71547953287761,
+            "unit": "ns",
+            "range": "± 4.215658874011142"
           }
         ]
       }
